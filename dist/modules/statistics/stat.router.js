7f5f4bf00d70ffeb931e9dd56f9d1bd435b78d6a
Tanvir Mahtab Tafhim <tafhim000001@gmail.com>
2026-01-31 11:35:24 +0600

    hh

--- dist/modules/statistics/stat.router.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { statsController } from "./stat.controller";
-const router = Router();
-router.get('/', statsController.getStat);
-export const statsRouter = router;
-//# sourceMappingURL=stat.router.js.map
